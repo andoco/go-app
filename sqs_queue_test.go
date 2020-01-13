f689f43f5be304a109174bc6a938447d5d685374
Andrew O'Connor <andrew.oconnor@gmail.com>
2020-01-13 17:17:30 +0000

    Test receiving messages

--- sqs_queue_test.go
+++ sqs_queue_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/net/context"
 )
 
@@ -32,6 +33,17 @@ func TestNewDeleteMessageInput(t *testing.T) {
 	assert.Equal(t, aws.String("test-handle"), dmi.ReceiptHandle)
 }
 
+func TestReceive(t *testing.T) {
+	output := &sqs.ReceiveMessageOutput{Messages: []*sqs.Message{{Body: aws.String("test-body")}}}
+	mockSvc := &mockSQSClient{output: output}
+	qconf := NewQueueConfig("msgType")
+	queue := NewQueue(qconf, mockSvc)
+	msgs, err := queue.Receive(context.TODO(), "foo", 1)
+	require.NoError(t, err)
+	require.Len(t, msgs, 1)
+	assert.Equal(t, "test-body", *msgs[0].Body)
+}
+
 func TestReceiveWhenCancelledReturnsNoMessages(t *testing.T) {
 	mockSvc := &mockSQSClient{err: awserr.New(request.CanceledErrorCode, "", nil)}
 	qconf := NewQueueConfig("msgType")
@@ -52,9 +64,13 @@ func TestReceiveWhenNonCancelledErrorReturnsError(t *testing.T) {
 
 type mockSQSClient struct {
 	sqsiface.SQSAPI
-	err error
+	err    error
+	output *sqs.ReceiveMessageOutput
 }
 
 func (m *mockSQSClient) ReceiveMessageWithContext(aws.Context, *sqs.ReceiveMessageInput, ...request.Option) (*sqs.ReceiveMessageOutput, error) {
+	if m.output != nil {
+		return m.output, nil
+	}
 	return nil, m.err
 }
